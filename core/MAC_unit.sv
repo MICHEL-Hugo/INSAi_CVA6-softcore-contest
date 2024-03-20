fa26578597973d9caee7fe2f0da105d740630ab9
Party Nell <party@insa-10580>
2024-03-20 10:46:58 +0100

    new version of MAC unit

--- core/MAC_unit.sv
+++ core/MAC_unit.sv
@@ -1,4 +1,3 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -19,89 +18,52 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-//riscv::xlen_t declaration
-package riscv;
-
-    localparam XLEN = 32;
-    typedef logic [XLEN-1:0] xlen_t;
-    
-endpackage
+//Implemented MAC unit is for signed operand A and unsigned operand B
 
 module MAC_unit(
-//    import ariane_pkg::*;
-//#(
-//    parameter config_pkg::cva6_cfg_t CVA6Cfg = config_pkg::cva6_cfg_empty
-//) 
-    input   logic           clk_i,
-    input   logic           rst_i,
-    input   riscv::xlen_t   operand_a_i, //Kernel (signed)
-    input   riscv::xlen_t   operand_b_i, //Image (unsigned)
-    output  riscv::xlen_t   result_o
+    import ariane_pkg::*;
+#(
+    parameter config_pkg::cva6_cfg_t CVA6Cfg = config_pkg::cva6_cfg_empty
+) 
+    input   logic                       clk_i,
+    input   logic                       rst_i,
+    //input   logic                       mac_valid_i,
+    input   fu_data_t       fu_data_i,
+    output  riscv::xlen_t               result_o
+    //output  logic                       mac_valid_i
     );
     
     //mulplication
-    logic [15:0] mult_o_1, mult_o_2, mult_o_3, mult_o_4; //intermediate registers
+    logic [15:0] mult_d_1, mult_d_2, mult_d_3, mult_d_4; //intermediate registers
+    logic [15:0] mult_q_1, mult_q_2, mult_q_3, mult_q_4; //intermediate registers
+    logic mac_valid_1;
+    
+    assign mult_d_1 = $signed({fu_data_i.operand_a[1*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[1*riscv::XLEN/4-1:0]}) 
+                        * $signed({fu_data_i.operand_b[1*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[1*riscv::XLEN/4-1:0]});
+                        
+    assign mult_d_2 = $signed({fu_data_i.operand_a[2*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[2*riscv::XLEN/4-1:1*riscv::XLEN/4]}) 
+                        * $signed({fu_data_i.operand_b[2*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[2*riscv::XLEN/4-1:1*riscv::XLEN/4]});
+                        
+    assign mult_d_3 = $signed({fu_data_i.operand_a[3*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[3*riscv::XLEN/4-1:2*riscv::XLEN/4]}) 
+                        * $signed({fu_data_i.operand_b[3*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[3*riscv::XLEN/4-1:2*riscv::XLEN/4]});
+                        
+    assign mult_d_4 = $signed({fu_data_i.operand_a[4*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[4*riscv::XLEN/4-1:3*riscv::XLEN/4]}) 
+                        * $signed({fu_data_i.operand_b[4*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[4*riscv::XLEN/4-1:3*riscv::XLEN/4]});
     
     always_ff @(posedge clk_i or negedge rst_i) begin
     
       if (~rst_i) begin
-        mult_o_1 <= '0;
-        //result_o <= '0; ON NE SAIT PAS A VOIR ENSEMBLE (JOHN, le dieu ultime du CVA6)
+        mult_q_1 <= 15'd0;
+        mult_q_2 <= 15'd0;
+        mult_q_3 <= 15'd0;
+        mult_q_4 <= 15'd0;
       end 
-      
-      else begin
-      
-        mult_o_1 <= $signed({operand_a_i[1*riscv::XLEN/4-1] & 1'b1, operand_a_i[1*riscv::XLEN/4-1:0]}) 
-                        * $signed({operand_b_i[1*riscv::XLEN/4-1] & 1'b0, operand_b_i[1*riscv::XLEN/4-1:0]});
-        //les assign variable = value n'étant pas acceptés nous avons mis des '<='
-      end
-    end
-    
-    always_ff @(posedge clk_i or negedge rst_i) begin
-    
-      if (~rst_i) begin
-        mult_o_2 <= '0;
-        //result_o <= '0; ON NE SAIT PAS A VOIR ENSEMBLE (JOHN, le dieu ultime du CVA6)
-      end 
-      
-      else begin
-      
-        mult_o_2 <= $signed({operand_a_i[2*riscv::XLEN/4-1] & 1'b1, operand_a_i[2*riscv::XLEN/4-1:1*riscv::XLEN/4]}) 
-                        * $signed({operand_b_i[2*riscv::XLEN/4-1] & 1'b0, operand_b_i[2*riscv::XLEN/4-1:1*riscv::XLEN/4]});
-     
-        //les assign variable = value n'étant pas acceptés nous avons mis des '<='
-      end
-    end
-    
-    always_ff @(posedge clk_i or negedge rst_i) begin
-    
-      if (~rst_i) begin
-        mult_o_3 <= '0;
-        //result_o <= '0; ON NE SAIT PAS A VOIR ENSEMBLE (JOHN, le dieu ultime du CVA6)
-      end 
-      
-      else begin
         
-        mult_o_3 <= $signed({operand_a_i[3*riscv::XLEN/4-1] & 1'b1, operand_a_i[3*riscv::XLEN/4-1:2*riscv::XLEN/4]}) 
-                        * $signed({operand_b_i[3*riscv::XLEN/4-1] & 1'b0, operand_b_i[3*riscv::XLEN/4-1:2*riscv::XLEN/4]});
-                        
-        //les assign variable = value n'étant pas acceptés nous avons mis des '<='
-      end
-    end
-    
-    always_ff @(posedge clk_i or negedge rst_i) begin
-    
-      if (~rst_i) begin
-        mult_o_4 <= '0;
-        //result_o <= '0; ON NE SAIT PAS A VOIR ENSEMBLE (JOHN, le dieu ultime du CVA6)
-      end 
-      
       else begin
-        mult_o_4 <= $signed({operand_a_i[4*riscv::XLEN/4-1] & 1'b1, operand_a_i[4*riscv::XLEN/4-1:3*riscv::XLEN/4]}) 
-                        * $signed({operand_b_i[4*riscv::XLEN/4-1] & 1'b0, operand_b_i[4*riscv::XLEN/4-1:3*riscv::XLEN/4]});
-                        
-        //les assign variable = value n'étant pas acceptés nous avons mis des '<='
+        mult_q_1 <= mult_d_1;
+        mult_q_2 <= mult_d_2;
+        mult_q_3 <= mult_d_3;
+        mult_q_4 <= mult_d_4;
       end
     end
     
@@ -109,53 +71,45 @@ module MAC_unit(
     
     
     //addition stage 1
-    logic [16:0] add_o_1, add_o_2; //intermediate registers
+    logic [16:0] add_q_1, add_q_2; //intermediate registers
+    logic [16:0] add_d_1, add_d_2; //intermediate registers
+    
+    assign add_d_1 = $signed({mult_q_1[15] & 1'b1, mult_q_1}) + $signed({mult_q_2[15] & 1'b1, mult_q_2});
+    assign add_d_2 = $signed({mult_q_3[15] & 1'b1, mult_q_3}) + $signed({mult_q_4[15] & 1'b1, mult_q_4});
     
     always_ff @(posedge clk_i or negedge rst_i) begin
     
       if (~rst_i) begin
-        add_o_1 <= '0;
-        //result_o <= '0; ON NE SAIT PAS A VOIR ENSEMBLE (JOHN, le dieu ultime du CVA6)
+        add_q_1 <= 16'd0;
+        add_q_2 <= 16'd0;
       end 
       
       else begin
-        add_o_1 <= $signed({mult_o_1[15] & 1'b1, mult_o_1}) + $signed({mult_o_2[15] & 1'b1, mult_o_2});          
-        //les assign variable = value n'étant pas acceptés nous avons mis des '<='
-      end
-    end
-    
-    always_ff @(posedge clk_i or negedge rst_i) begin
-    
-      if (~rst_i) begin
-        add_o_2 <= '0;
-        //result_o <= '0; ON NE SAIT PAS A VOIR ENSEMBLE (JOHN, le dieu ultime du CVA6)
-      end 
-      
-      else begin
-        add_o_2 <= $signed({mult_o_3[15] & 1'b1, mult_o_3}) + $signed({mult_o_4[15] & 1'b1, mult_o_4});             
-        //les assign variable = value n'étant pas acceptés nous avons mis des '<='
+        add_q_1 <= add_d_1;
+        add_q_2 <= add_d_2; 
       end
     end
     
     //mise en place de signaux avertissant que les opérations d'addition sont terminés
     
     //addition stage 2
-    logic [17:0] add_o_3; //intermediate registers
+    logic [17:0] add_q_3; //intermediate registers
+    logic [17:0] add_d_3;
+    
+    assign add_d_3 = $signed({add_q_1[16] & 1'b1, add_q_1}) + $signed({add_q_2[15] & 1'b1, add_q_2});
     
     always_ff @(posedge clk_i or negedge rst_i) begin
     
       if (~rst_i) begin
-        add_o_3 <= '0;
-        //result_o <= '0; ON NE SAIT PAS A VOIR ENSEMBLE (JOHN, le dieu ultime du CVA6)
+            add_q_3 <= 17'd0;
       end 
       
       else begin
-        add_o_3 <= $signed({add_o_1[16] & 1'b1, add_o_1}) + $signed({add_o_2[15] & 1'b1, add_o_2}); ;         
-        //les assign variable = value n'étant pas acceptés nous avons mis des '<='
+            add_q_3 <= add_d_3;
       end
     end
     
-    assign result_o = 32'(signed'(add_o_3));
+    assign result_o = 32'(signed'(add_q_3));
     
     
     
