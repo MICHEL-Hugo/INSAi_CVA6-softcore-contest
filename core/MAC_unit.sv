73f8022508c1af87e9327187b0f9282003d82dc8
Party Nell <party@insa-20554>
2024-03-21 16:32:51 +0100

    add trans_id signals to the MAC unit

--- core/MAC_unit.sv
+++ core/MAC_unit.sv
@@ -1,4 +1,3 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -6,7 +5,7 @@
 // Create Date: 03/15/2024 05:23:32 PM
 // Design Name: 
 // Module Name: MAC_unit
-// Project Name: cva6-softcore-contest
+// Project Name: 
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -19,24 +18,25 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 //Implemented MAC unit is for signed operand A and unsigned operand B
 localparam VALID = 1'b1;
 localparam READY = 1'b1;
 
 module MAC_unit(
-    import ariane_pkg::*;
-#(
-    parameter config_pkg::cva6_cfg_t CVA6Cfg = config_pkg::cva6_cfg_empty
-) 
+//    import ariane_pkg::*;
+//#(
+//    parameter config_pkg::cva6_cfg_t CVA6Cfg = config_pkg::cva6_cfg_empty
+//) 
     input   logic                       clk_i,
     input   logic                       rst_i,
     input   logic                       mac_valid_i,
     input   logic                       flush_i,
+    input   logic   [TRANS_ID_BITS-1:0] trans_id_i,
     input   ariane_pkg::fu_data_t       fu_data_i,
     output  riscv::xlen_t               result_o,
     output  logic                       mac_valid_o,
-    output  logic                       mac_ready_o
+    output  logic                       mac_ready_o,
+    output  logic   [TRANS_ID_BITS-1:0] trans_id_o
     );
     
     //control signals
@@ -44,8 +44,10 @@ module MAC_unit(
     
     //mulplication
     logic [15:0] mult_d_1, mult_d_2, mult_d_3, mult_d_4; //intermediate registers
-    logic [15:0] mult_q_1, mult_q_2, mult_q_3, mult_q_4; //intermediate registers
+    logic [15:0] mult_q_1, mult_q_2, mult_q_3, mult_q_4;
+    
     logic mac_valid_1_q, mac_valid_1_d;
+    logic   [TRANS_ID_BITS-1:0] trans_id_1_q;
     
     assign mult_d_1 = $signed({fu_data_i.operand_a[1*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[1*riscv::XLEN/4-1:0]}) 
                         * $signed({fu_data_i.operand_b[1*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[1*riscv::XLEN/4-1:0]});
@@ -70,6 +72,7 @@ module MAC_unit(
         mult_q_4 <= 15'd0;
         
         mac_valid_1_q <= ~VALID;
+        trans_id_1_q <= '0;
       end 
         
       else begin
@@ -80,10 +83,12 @@ module MAC_unit(
             mult_q_4 <= mult_d_4;
             
             mac_valid_1_q <= mac_valid_1_d;
+            trans_id_1_q <= trans_id_i;
         end 
         
         else begin
             mac_valid_1_q <= ~VALID;
+            trans_id_1_q <= '0;
             
             mult_q_1 <= 15'd0;
             mult_q_2 <= 15'd0;
@@ -98,8 +103,11 @@ module MAC_unit(
     
     //addition stage 1
     logic [16:0] add_q_1, add_q_2; //intermediate registers
-    logic [16:0] add_d_1, add_d_2; 
+    logic [16:0] add_d_1, add_d_2;
+    
     logic mac_valid_2_q, mac_valid_2_d;
+    logic   [TRANS_ID_BITS-1:0] trans_id_2_q;
+    
     
     assign add_d_1 = $signed({mult_q_1[15] & 1'b1, mult_q_1}) + $signed({mult_q_2[15] & 1'b1, mult_q_2});
     assign add_d_2 = $signed({mult_q_3[15] & 1'b1, mult_q_3}) + $signed({mult_q_4[15] & 1'b1, mult_q_4});
@@ -113,6 +121,7 @@ module MAC_unit(
         add_q_2 <= 16'd0;
         
         mac_valid_2_q <= ~VALID;
+        trans_id_2_q <= '0;
       end 
       
       else begin
@@ -122,10 +131,12 @@ module MAC_unit(
             add_q_2 <= add_d_2;
             
             mac_valid_2_q <= mac_valid_2_d;
+            trans_id_2_q <= trans_id_1_q;
         end 
         
         else begin
             mac_valid_2_q <= ~VALID;
+            trans_id_2_q <= '0;
             
             add_q_1 <= 16'd0;
             add_q_2 <= 16'd0;
@@ -140,6 +151,7 @@ module MAC_unit(
     logic [17:0] add_d_3;
     
     logic mac_valid_3_q, mac_valid_3_d;
+    logic   [TRANS_ID_BITS-1:0] trans_id_3_q;
 
     
     assign add_d_3 = $signed({add_q_1[16] & 1'b1, add_q_1}) + $signed({add_q_2[15] & 1'b1, add_q_2});
@@ -152,6 +164,7 @@ module MAC_unit(
             add_q_3 <= 17'd0;
             
             mac_valid_3_q <= ~VALID;
+            trans_id_3_q <= '0;
       end 
       
       else begin
@@ -159,11 +172,13 @@ module MAC_unit(
             add_q_3 <= add_d_3;
             
             mac_valid_3_q <= mac_valid_3_d;
+            trans_id_3_q <= trans_id_2_q;
         end 
         
         else begin
             mac_valid_3_q <= ~VALID;
-            
+            trans_id_3_q <= '0;
+
             add_q_3 <= 17'd0;
         end
       end
@@ -171,6 +186,7 @@ module MAC_unit(
     
     assign result_o = 32'(signed'(add_q_3));
     assign mac_valid_o = mac_valid_3_q;
+    assign trans_id_o = trans_id_3_q;
     
     
     
