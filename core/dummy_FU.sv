0c17b9bab9b4e5028d6682e37cea4b23a9c0ca8e
diskouna <diskounajohn.gnanguessim@uit.ac.ma>
2024-03-30 09:44:18 +0100

    refactor dummy_FU implementation

--- core/dummy_FU.sv
+++ core/dummy_FU.sv
@@ -18,19 +18,18 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-localparam TRANS_ID_BITS = 4;
 
 //Implemented MAC unit is for signed operand A and unsigned operand B
 localparam VALID = 1'b1;
 localparam READY = 1'b1;
 
-module dummy_FU(
+module dummy_FU
     import ariane_pkg::*;
 #(
     parameter config_pkg::cva6_cfg_t CVA6Cfg = config_pkg::cva6_cfg_empty
-) 
+) (
     input   logic                       clk_i,
-    input   logic                       rst_i,
+    input   logic                       rst_ni,
     input   logic                       dummy_FU_valid_i,
     input   logic                       flush_i,
     input   ariane_pkg::fu_data_t       fu_data_i,
@@ -50,56 +49,23 @@ module dummy_FU(
     logic [15:0] mult_q_1, mult_q_2, mult_q_3, mult_q_4;
     
     logic mac_valid_1_q, mac_valid_1_d;
-    logic   [TRANS_ID_BITS-1:0] trans_id_1_q;
+    logic   [TRANS_ID_BITS-1:0] trans_id_1_q, trans_id_1_d;
     
-    assign mult_d_1 = $signed({fu_data_i.operand_a[1*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[1*riscv::XLEN/4-1:0]}) 
-                        * $signed({fu_data_i.operand_b[1*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[1*riscv::XLEN/4-1:0]});
+    assign mult_d_1 = $signed({fu_data_i.operand_a[1*riscv::XLEN/4-1], fu_data_i.operand_a[1*riscv::XLEN/4-1:0]}) 
+                        * $signed({1'b0, fu_data_i.operand_b[1*riscv::XLEN/4-1:0]});
                         
-    assign mult_d_2 = $signed({fu_data_i.operand_a[2*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[2*riscv::XLEN/4-1:1*riscv::XLEN/4]}) 
-                        * $signed({fu_data_i.operand_b[2*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[2*riscv::XLEN/4-1:1*riscv::XLEN/4]});
+    assign mult_d_2 = $signed({fu_data_i.operand_a[2*riscv::XLEN/4-1], fu_data_i.operand_a[2*riscv::XLEN/4-1:1*riscv::XLEN/4]}) 
+                        * $signed({1'b0, fu_data_i.operand_b[2*riscv::XLEN/4-1:1*riscv::XLEN/4]});
                         
-    assign mult_d_3 = $signed({fu_data_i.operand_a[3*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[3*riscv::XLEN/4-1:2*riscv::XLEN/4]}) 
-                        * $signed({fu_data_i.operand_b[3*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[3*riscv::XLEN/4-1:2*riscv::XLEN/4]});
+    assign mult_d_3 = $signed({fu_data_i.operand_a[3*riscv::XLEN/4-1], fu_data_i.operand_a[3*riscv::XLEN/4-1:2*riscv::XLEN/4]}) 
+                        * $signed({1'b0, fu_data_i.operand_b[3*riscv::XLEN/4-1:2*riscv::XLEN/4]});
                         
-    assign mult_d_4 = $signed({fu_data_i.operand_a[4*riscv::XLEN/4-1] & 1'b1, fu_data_i.operand_a[4*riscv::XLEN/4-1:3*riscv::XLEN/4]}) 
-                        * $signed({fu_data_i.operand_b[4*riscv::XLEN/4-1] & 1'b0, fu_data_i.operand_b[4*riscv::XLEN/4-1:3*riscv::XLEN/4]});
+    assign mult_d_4 = $signed({fu_data_i.operand_a[4*riscv::XLEN/4-1], fu_data_i.operand_a[4*riscv::XLEN/4-1:3*riscv::XLEN/4]}) 
+                        * $signed({1'b0, fu_data_i.operand_b[4*riscv::XLEN/4-1:3*riscv::XLEN/4]});
     
-    assign mac_valid_1_d = dummy_FU_valid_i && ~flush_i; //result are valid if the unit has been choosen and if the pipeline is not flushed
+    assign mac_valid_1_d = ~flush_i & dummy_FU_valid_i; //result are valid if the unit has been choosen and if the pipeline is not flushed
     
-    always_ff @(posedge clk_i or negedge rst_i) begin
-    
-      if (~rst_i) begin
-        mult_q_1 <= 15'd0;
-        mult_q_2 <= 15'd0;
-        mult_q_3 <= 15'd0;
-        mult_q_4 <= 15'd0;
-        
-        mac_valid_1_q <= ~VALID;
-        trans_id_1_q <= '0;
-      end 
-        
-      else begin
-        if (mac_valid_1_d == VALID && ~flush_i) begin
-            mult_q_1 <= mult_d_1;
-            mult_q_2 <= mult_d_2;
-            mult_q_3 <= mult_d_3;
-            mult_q_4 <= mult_d_4;
-            
-            mac_valid_1_q <= mac_valid_1_d;
-            trans_id_1_q <= fu_data_i.trans_id;
-        end 
-        
-        else begin
-            mac_valid_1_q <= ~VALID;
-            trans_id_1_q <= '0;
-            
-            mult_q_1 <= 15'd0;
-            mult_q_2 <= 15'd0;
-            mult_q_3 <= 15'd0;
-            mult_q_4 <= 15'd0;
-        end
-      end
-    end
+
     
     //mise en place de signaux avertissant que les opérations de multiplications sont terminés
     
@@ -109,43 +75,13 @@ module dummy_FU(
     logic [16:0] add_d_1, add_d_2;
     
     logic mac_valid_2_q, mac_valid_2_d;
-    logic   [TRANS_ID_BITS-1:0] trans_id_2_q;
+    logic   [TRANS_ID_BITS-1:0] trans_id_2_q, trans_id_2_d;
     
     
-    assign add_d_1 = $signed({mult_q_1[15] & 1'b1, mult_q_1}) + $signed({mult_q_2[15] & 1'b1, mult_q_2});
-    assign add_d_2 = $signed({mult_q_3[15] & 1'b1, mult_q_3}) + $signed({mult_q_4[15] & 1'b1, mult_q_4});
+    assign add_d_1 = $signed({mult_q_1[15], mult_q_1}) + $signed({mult_q_2[15], mult_q_2});
+    assign add_d_2 = $signed({mult_q_3[15], mult_q_3}) + $signed({mult_q_4[15], mult_q_4});
     
-    assign mac_valid_2_d = mac_valid_1_q && ~flush_i;
-    
-    always_ff @(posedge clk_i or negedge rst_i) begin
-    
-      if (~rst_i) begin
-        add_q_1 <= 16'd0;
-        add_q_2 <= 16'd0;
-        
-        mac_valid_2_q <= ~VALID;
-        trans_id_2_q <= '0;
-      end 
-      
-      else begin
-      
-        if (mac_valid_2_d == VALID && ~flush_i) begin
-            add_q_1 <= add_d_1;
-            add_q_2 <= add_d_2;
-            
-            mac_valid_2_q <= mac_valid_2_d;
-            trans_id_2_q <= trans_id_1_q;
-        end 
-        
-        else begin
-            mac_valid_2_q <= ~VALID;
-            trans_id_2_q <= '0;
-            
-            add_q_1 <= 16'd0;
-            add_q_2 <= 16'd0;
-        end
-      end
-    end
+    assign mac_valid_2_d = ~flush_i & mac_valid_1_q;
     
     //mise en place de signaux avertissant que les opérations d'addition sont terminés
     
@@ -154,44 +90,57 @@ module dummy_FU(
     logic [17:0] add_d_3;
     
     logic mac_valid_3_q, mac_valid_3_d;
-    logic   [TRANS_ID_BITS-1:0] trans_id_3_q;
+    logic   [TRANS_ID_BITS-1:0] trans_id_3_q, trans_id_3_d;
 
     
-    assign add_d_3 = $signed({add_q_1[16] & 1'b1, add_q_1}) + $signed({add_q_2[15] & 1'b1, add_q_2});
+    assign add_d_3 = $signed({add_q_1[16], add_q_1}) + $signed({add_q_2[16], add_q_2});
     
-    assign mac_valid_3_d = mac_valid_2_q && ~flush_i;
+    assign mac_valid_3_d = ~flush_i & mac_valid_2_q;
     
-    always_ff @(posedge clk_i or negedge rst_i) begin
     
-      if (~rst_i) begin
-            add_q_3 <= 17'd0;
-            
-            mac_valid_3_q <= ~VALID;
-            trans_id_3_q <= '0;
-      end 
+    assign trans_id_3_d        = trans_id_2_q;
+    assign trans_id_2_d        = trans_id_1_q;
+    assign trans_id_1_d        = fu_data_i.trans_id;
       
-      else begin
-      if (mac_valid_3_d == VALID && ~flush_i) begin
-            add_q_3 <= add_d_3;
-            
-            mac_valid_3_q <= mac_valid_3_d;
-            trans_id_3_q <= trans_id_2_q;
-        end 
-        
-        else begin
-            mac_valid_3_q <= ~VALID;
-            trans_id_3_q <= '0;
-
-            add_q_3 <= 17'd0;
-        end
-      end
-    end
-    
-    assign dummy_FU_result_o = 32'(signed'(add_q_3));
-    assign dummy_FU_valid_o = mac_valid_3_q;
+    assign dummy_FU_result_o   = 32'(signed'(add_q_3));
+    assign dummy_FU_valid_o    = mac_valid_3_q;
     assign dummy_FU_trans_id_o = trans_id_3_q;
     
-    
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+      if (~rst_ni) begin
+        mult_q_1 <= '0;
+        mult_q_2 <= '0;
+        mult_q_3 <= '0;
+        mult_q_4 <= '0;       
+        mac_valid_1_q <= ~VALID;
+        trans_id_1_q <= '0;
+      
+        add_q_1 <= '0;
+        add_q_2 <= '0;
+        mac_valid_2_q <= ~VALID;
+        trans_id_2_q <= '0;
+     
+        add_q_3 <= '0;    
+        mac_valid_3_q <= ~VALID;
+        trans_id_3_q <= '0;
+      end else begin 
+        mult_q_1      <= mult_d_1;
+        mult_q_2      <= mult_d_2;
+        mult_q_3      <= mult_d_3;
+        mult_q_4      <= mult_d_4;         
+        mac_valid_1_q <= mac_valid_1_d;
+        trans_id_1_q  <= trans_id_1_d;
+      
+        add_q_1       <= add_d_1;
+        add_q_2       <= add_d_2;    
+        mac_valid_2_q <= mac_valid_2_d;
+        trans_id_2_q  <= trans_id_2_d;
+      
+        add_q_3       <= add_d_3;
+        mac_valid_3_q <= mac_valid_3_d;
+        trans_id_3_q  <= trans_id_3_d;
+      end
+    end
     
 endmodule
 
