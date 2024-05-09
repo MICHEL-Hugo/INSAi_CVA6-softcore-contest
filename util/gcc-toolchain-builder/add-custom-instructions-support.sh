f0f3d31fe9d206a13fd7b752088eca0f15ce0f85
Achoul <hcaute@gmail.com>
2024-05-09 11:42:49 +0200

    add-custom-...1

--- util/gcc-toolchain-builder/add-custom-instructions-support.sh
+++ util/gcc-toolchain-builder/add-custom-instructions-support.sh
@@ -24,12 +24,12 @@ MASK_MAC8_ACC="#define MASK_MAC8_ACC 0xfe00707f"
 MATCH_MAC8_INIT="#define MATCH_MAC8_INIT 0x200b"
 MASK_MAC8_INIT="#define MASK_MAC8_INIT 0xfe00707f"
 MATCH_MIX="#define MATCH_MIX 0x200c"
-MASK_MIX="#define MASK_MIX  0x707f"
+MASK_MIX="#define MASK_MIX  0xfe00707f"
 
 
 DECLARE_INSN_MAC8_ACC="DECLARE_INSN(mac8_acc, MATCH_MAC8_ACC	, MASK_MAC8_ACC)"
 DECLARE_INSN_MAC8_INIT="DECLARE_INSN(mac8_init, MATCH_MAC8_INIT	, MASK_MAC8_INIT)"
-DECLARE_INSN="DECLARE_INS(mix, MATCH_MIX	, MASK_MIX)"
+DECLARE_INSN_MIX="DECLARE_INSN(mix, MATCH_MIX	, MASK_MIX)"
 
 # riscv-opc.c
 
@@ -40,35 +40,21 @@ MIX_OPCODE="{\"mix\",         0, INSN_CLASS_I, \"d,s,t\",     MATCH_MIX, MASK_MI
 
 echo "[insAI] adding mac8_acc and mac8_init instruction support...";
 
-grep  -w "MASK_MAC8_ACC\|MASK_MAC8_INIT"  $riscv_opc_h 1>/dev/null 2>&1;
+grep  -w "MASK_MAC8_ACC\|MASK_MAC8_INIT\|MASK_MIX"  $riscv_opc_h 1>/dev/null 2>&1;
 if [ $? -eq 1 ]; then
-	sed -i '/#define RISCV_ENCODING_H/,/#endif \/\* RISCV_ENCODING_H/  s/^\/\* Instruction.*$/&\n\n'"$HEADER\n$MATCH_MAC8_ACC\n$MASK_MAC8_ACC\n"'\n/i' $riscv_opc_h 
-	sed -i '/#define RISCV_ENCODING_H/,/#endif \/\* RISCV_ENCODING_H/  s/^\/\* Instruction.*$/&\n\n'"$HEADER\n$MATCH_MAC8_INIT\n$MASK_MAC8_INIT\n"'\n/i' $riscv_opc_h 
-	sed -i '/#ifdef DECLARE_INSN/, /#endif \/\* DECLARE_INSN \*\// s/#endif \/\* DECLARE_INSN \*\//'"$HEADER\n$DECLARE_INSN_MAC8_ACC\n\n"'&/i' $riscv_opc_h 
-		sed -i '/#ifdef DECLARE_INSN/, /#endif \/\* DECLARE_INSN \*\// s/#endif \/\* DECLARE_INSN \*\//'"$HEADER\n$DECLARE_INSN_MAC8_INIT\n\n"'&/i' $riscv_opc_h 
+	sed -i '/#define RISCV_ENCODING_H/,/#endif \/\* RISCV_ENCODING_H/  s/^\/\* Instruction.*$/&\n\n'        \
+        "$HEADER\n$MATCH_MAC8_ACC\n$MASK_MAC8_ACC\n$MATCH_MAC8_INIT\n$MASK_MAC8_INIT\n$MATCH_MIX\n$MASK_MIX\n"'\n/i' $riscv_opc_h 
+	sed -i '/#ifdef DECLARE_INSN/, /#endif \/\* DECLARE_INSN \*\// s/#endif \/\* DECLARE_INSN \*\//'        \
+        "$HEADER\n$DECLARE_INSN_MAC8_ACC\n$DECLARE_INSN_MAC8_INIT\n$DECLARE_INSN_MIX\n\n"'&/i' $riscv_opc_h 
 else
-	echo "[insAI] mac8.acc and mac8.init instruction is already present in $riscv_opc_h"
+	echo "[insAI] mac8_init or mac8_acc is already present in $riscv_opc_h"
 fi	
 
-grep  -w "mac8.acc\|mac8.init" $riscv_opc_c 1>/dev/null 2>&1
+grep  -w "mac8_acc\|mac8_init" $riscv_opc_c 1>/dev/null 2>&1
 if [ $? -eq 1 ]; then
-	sed -i '/riscv_opcodes/,/^};/ s/^\/\* Terminate/'"$HEADER\n$MAC8_ACC_OPCODE\n"'\n&/i' $riscv_opc_c  
-	sed -i '/riscv_opcodes/,/^};/ s/^\/\* Terminate/'"$HEADER\n$MAC8_INIT_OPCODE\n"'\n&/i' $riscv_opc_c  
+	sed -i '/riscv_opcodes/,/^};/ s/^\/\* Terminate/'"$HEADER\n$MAC8_ACC_OPCODE\n$MAC8_INIT_OPCODE\n$MIX_OPCODE\n"'\n&/i' $riscv_opc_c  
 else
-	echo "[insAI] mac8 instruction is already present in $riscv_opc_c"
+	echo "[insAI] mac8_init or mac8_acc or mix instructions is already present in $riscv_opc_c"
 fi
 
-grep  -w mix  $riscv_opc_h 1>/dev/null 2>&1;
-if [ $? -eq 1 ]; then
-	sed -i '/#define RISCV_ENCODING_H/,/#endif \/\* RISCV_ENCODING_H/  s/^\/\* Instruction.*$/&\n\n'"$HEADER\n$MATCH_MIX\n$MASK_MIX\n"'\n/i' $riscv_opc_h 
-	sed -i '/#ifdef DECLARE_INSN/, /#endif \/\* DECLARE_INSN \*\// s/#endif \/\* DECLARE_INSN \*\//'"$HEADER\n$DECLARE_INSN_MIX\n\n"'&/i' $riscv_opc_h 
-else
-	echo "[insAI] mix instruction is already present in $riscv_opc_h"
-fi	
 
-grep  -w mix $riscv_opc_c 1>/dev/null 2>&1
-if [ $? -eq 1 ]; then
-	sed -i '/riscv_opcodes/,/^};/ s/^\/\* Terminate/'"$HEADER\n$MIX_OPCODE\n"'\n&/i' $riscv_opc_c  
-else
-	echo "[insAI] mix instruction is already present in $riscv_opc_c"
-fi
