1814f372e3ca969d6ae0cb58d00243805a5a9d48
diskouna <diskounajohn.gnanguessim@uit.ac.ma>
2024-05-09 21:02:43 +0200

    remove unused parameters

--- sw/app/mnist/params.h
+++ sw/app/mnist/params.h
@@ -8,11 +8,7 @@
 #define UNSIGNED_DATA 1
 
 // insAI configuration
-#define ENABLE_insAI_EXTENSION 1 
-#define HOST_IS_FPGA 1 
-
 #define USE_MAC_PACK32 1
 #define USE_BLOCK_SIZE_16  1
-#define BLOCK_SIZE_4   0
 
 #endif // N2D2_EXPORTC_PARAMS_H
